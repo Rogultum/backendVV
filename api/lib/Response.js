f1460efdc2ffb4d59eb082e8b2afb0c6a80f76cf
Remzi Ogul TUM <amadeuwoles@gmail.com>
2023-07-05 17:34:51 +0300

    add custom error response

--- api/lib/Response.js
+++ api/lib/Response.js
@@ -1,3 +1,6 @@
+const Enum = require("../config/Enum");
+const CustomError = require("./Error");
+
 class Response {
   constructor() {}
   static successResponse(data, code = 200) {
@@ -5,11 +8,21 @@ class Response {
   }
 
   static errorResponse(error) {
+    if (error instanceof CustomError) {
+      return {
+        code: error.code,
+        error: {
+          message: error.message,
+          description: error.description,
+        },
+      };
+    }
+
     return {
-      code,
+      code: Enum.HTTP_CODES.INTERNAL_SERVER_ERROR,
       error: {
-        message: error.message,
-        description: error.description,
+        message: "Unknown Error",
+        description: error.message,
       },
     };
   }
