20e3ba5552bc776577c349ecb76f36de920ce9a2
Remzi Ogul TUM <amadeuwoles@gmail.com>
2023-07-06 16:21:05 +0300

    add crud for roles

--- api/routes/roles.js
+++ api/routes/roles.js
@@ -1,13 +1,92 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  res.json({
-    body: req.body,
-    params: req.params,
-    query: req.query,
-    headers: req.headers,
-  });
+const Roles = require("../db/models/Roles");
+const RolePrivileges = require("../db/models/RolePrivileges");
+const Response = require("../lib/Response");
+const CustomError = require("../lib/Error");
+const Enum = require("../config/Enum");
+
+router.get("/", async (req, res, next) => {
+  try {
+    let roles = await Roles.find({});
+
+    res.json(Response.successResponse(roles));
+  } catch (error) {
+    let errorResponse = Response.errorResponse(error);
+    res.status(errorResponse.code).json(errorResponse);
+  }
+});
+
+router.post("/add", async (req, res, next) => {
+  let body = req.body;
+
+  try {
+    if (!body.role_name)
+      throw new CustomError(
+        Enum.HTTP_CODES.BAD_REQUEST,
+        "validation error",
+        "role_name must be filled."
+      );
+
+    let role = new Roles({
+      role_name: body.role_name,
+      is_active: true,
+      created_by: req.user?.id,
+    });
+
+    await role.save();
+
+    res.json(Response.successResponse({ success: true }));
+  } catch (error) {
+    let errorResponse = Response.errorResponse(error);
+    res.status(errorResponse.code).json(errorResponse);
+  }
+});
+
+router.post("/update", async (req, res, next) => {
+  let body = req.body;
+
+  try {
+    if (!body._id)
+      throw new CustomError(
+        Enum.HTTP_CODES.BAD_REQUEST,
+        "validation error",
+        "_id must be filled."
+      );
+
+    let updates = {};
+
+    if (body.role_name) updates.role_name = body.role_name;
+    if (typeof body.is_active === "boolean") updates.is_active = body.is_active;
+
+    await Roles.updateOne({ _id: body._id }, updates);
+
+    res.json(Response.successResponse({ success: true }));
+  } catch (error) {
+    let errorResponse = Response.errorResponse(error);
+    res.status(errorResponse.code).json(errorResponse);
+  }
+});
+
+router.post("/delete", async (req, res, next) => {
+  let body = req.body;
+
+  try {
+    if (!body._id)
+      throw new CustomError(
+        Enum.HTTP_CODES.BAD_REQUEST,
+        "validation error",
+        "_id must be filled."
+      );
+
+    await Roles.deleteOne({ _id: body._id });
+
+    res.json(Response.successResponse({ success: true }));
+  } catch (error) {
+    let errorResponse = Response.errorResponse(error);
+    res.status(errorResponse.code).json(errorResponse);
+  }
 });
 
 module.exports = router;
